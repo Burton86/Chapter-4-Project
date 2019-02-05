38b7fa7a2b94209b180e5f6d748d2e4200e4437a
Burton <KB145350@davenportschools.org>
2019-02-05 10:04:49 -0600

    update

--- src/CarInsurancePolicy2.java
+++ src/CarInsurancePolicy2.java
@@ -1,15 +1,25 @@
 public class CarInsurancePolicy2 {
-    public class CreatePolicies
-    {
-        public static void main(String[] args)
-        {
-            CarInsurancePolicy first = new CarInsurancePolicy(123);
-            CarInsurancePolicy second = new CarInsurancePolicy(456, 4);
-            CarInsurancePolicy thrid = new CarInsurancePolicy(789, 12, "Newcastle");
+    private int policyNumber;
+    private int numPayments;
+    private String residentCity;
 
-            first.display();
-            second.display();
-            thrid.display();
-        }
+    public CarInsurancePolicy2(int num, int payments, String city)
+    {
+        policyNumber = num;
+        numPayments = payments;
+        residentCity = city;
     }
+    public CarInsurancePolicy2(int num, int payments)
+    {
+        this(num, payments, city "MayField");
+    }
+    public CarInsurancePolicy2(int num)
+    {
+        this(num, payments2, city "MayField");
+    }
+    public void display()
+    {
+        System.out.println("Policy #" + policyNumber + "." + numPayments + "payments annually. Driver resides in" + residentCity + ".");
+    }
+
 }
